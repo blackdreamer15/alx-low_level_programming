2310234552eba6cc9d9059aa8cb4af9d2c883e20
Baki Jessy Justice Julien <bakijessy@gmail.com>
2022-12-09 14:17:22 +0000

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,17 +7,14 @@
  */
 int main(void)
 {
-char letter = 'a';
-while (letter <= 'z')
+char letter;
+for(letter='a'; letter <= 'z'; letter++;)
 {
 putchar(letter);
-letter++;
 }
-letter = 'A';
-while (c <= 'Z')
+for(letter='A'; letter <= 'Z'; letter++;)
 {
-putchar(c);
-letter++;
+putchar(letter);
 }
 putchar('\n');
 return (0);
