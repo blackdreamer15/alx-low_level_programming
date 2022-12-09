a723ac6fd9158aedbb7be778e174d36bcd717e21
Baki Jessy Justice Julien <bakijessy@gmail.com>
2022-12-09 16:07:14 +0000

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -27,9 +27,7 @@ int main(void)
 {
 int x;
 int y;
-int z;
-  
-  
+int z; 
 for (x = 0; x <= 9; x++)
 {
 for (y = 0; y <= 9; y++)
@@ -41,7 +39,7 @@ if (z != y && y != x && x < y && y < z)
 putchar('0' + x);
 putchar('0' + y);
 putchar('0' + z);
-if ( (x + y + z) != (9 + 8 + 7) )
+if ((x + y + z) != (9 + 8 + 7))
 {
 putchar(44);
 putchar(32);
