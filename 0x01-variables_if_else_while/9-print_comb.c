72d1cbb9e7e61d919eeb75fa0b98117880802c03
Baki Jessy Justice Julien <bakijessy@gmail.com>
2022-12-09 15:11:28 +0000

    Create 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - main entry of the code
+ * Description:  prints all 
+ * possible combinations of 
+ * single-digit numbers.
+ * Return: 0
+ */
+int main(void)
+{
+char num;
+for (num = 0; num <= 9; num++)
+{
+putchar(num+48);
+if (num!=9)
+{
+putchar(44);
+putchar(32);
+}
+}
+putchar('\n');
+return (0);
+}
