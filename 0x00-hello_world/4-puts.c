2a45396ae9b673315d286dfb5d1780956ba4a7c9
Baki Jessy Justice Julien <bakijessy@gmail.com>
2022-12-09 11:14:18 +0000

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Description: prints: "Programming is like building a multilingual puzzle
- * Return: 0
+ * main - prints: "Programming is like building a multilingual puzzle
+ * Return: 0 (success)
  */
 int main(void)
 {
