2fe2ac022c1f988d0a3e5cfa2e0232ce14bee72a
Baki Jessy Justice Julien <bakijessy@gmail.com>
2022-12-09 14:14:48 +0000

    Create 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description:  prints alphabet in lowercase, and
+ * then in uppercase, followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+putchar(letter);
+letter++;
+}
+letter = 'A';
+while (c <= 'Z')
+{
+putchar(c);
+letter++;
+}
+putchar('\n');
+return (0);
+}
