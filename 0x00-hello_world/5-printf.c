eccf4a861130b680ca15262244974b81d1abce0e
Baki Jessy Justice Julien <bakijessy@gmail.com>
2022-12-09 11:02:18 +0000

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: prints: "with proper grammar, but the outcome is a piece of art,\n"
+ * Description:print: with proper grammar, but the outcome is a piece of art,\n
  * Return: 0
  */
 int main(void)
