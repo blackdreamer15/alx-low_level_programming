8a4ab299fa004d497b4f3303886319be03b1a206
Baki Jessy Justice Julien <bakijessy@gmail.com>
2022-12-12 15:01:46 +0000

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -25,7 +25,7 @@ for (counter2 = 0; counter2 < 100; counter2++)
 {
 firstDigit_n2 = (counter2 / 10 + '0');
 lastDigit_n2 = (counter2 % 10 + '0');
-if (counter1 < counter 2)
+if (counter1 < counter2)
 {
 putchar(firstDigit_n1);
 putchar(lastDigit_n1);
