4a8dfc2201b84e13d3eb1a7a76c766ac33d5517e
Baki Jessy Justice Julien <bakijessy@gmail.com>
2022-12-12 14:58:20 +0000

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints the alphabet in lowercase\n
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter<= 'z'; letter++)
+{
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}
