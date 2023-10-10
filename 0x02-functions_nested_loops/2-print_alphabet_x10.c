aeb85b91f0e939f1118bb342e9e65596bde37c7f
Jessy Baki <bakijessy@gmail.com>
2023-10-10 05:56:02 +0000

    Add changes to 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,10 @@
 #include "main.h"
 
 /**
- * Description - Print all letters of the alphabet in lowercase
- * 10 times each followed by a new line
+ * print_alphabet_x10 - A function that prints all letters
+ * of the alphabet in lowercase 10 times each followed by a new line
+ *
+ * Return - void
  */
 
 void print_alphabet_x10(void)
