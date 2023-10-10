fe47ee1d8a42bb5a98aecbc2434c0d9f46397b49
Jessy Baki <bakijessy@gmail.com>
2023-10-10 06:24:30 +0000

    Add function to print times table from 0 to 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * times_table -  Prints times table.
+ * Return: number
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+
+		for (j = 1; j < 10; j++)
+		{
+			int p = i * j;
+
+			if (p < 10)
+				_putchar(' ');
+			else if (p > 9)
+				_putchar((p / 10) + '0');
+			
+			_putchar((p % 10) + '0');
+			
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+}
+}
