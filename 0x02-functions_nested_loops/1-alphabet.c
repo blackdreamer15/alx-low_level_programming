7762a91c8ec27dbe3f1769689abf77fdebfe3401
Jessy Baki <bakijessy@gmail.com>
2023-10-10 05:21:25 +0000

    Add fixes to 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,15 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
  * Description - Print all letters of the alphabet in lowercase
  * followed by a new line
- *
- * Return: Always 0 (Success)
  */
 
-int main(void)
+int print_alphabet(void)
 {
 	int letter;
 
 	for (letter = 97; letter < 123; letter++)
 		_putchar(letter);
 	_putchar('\n');
-
-	return (0);
 }
