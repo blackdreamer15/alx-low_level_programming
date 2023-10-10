31c615b494f5e7488d938f2bb501b4f0d47373c2
Jessy Baki <bakijessy@gmail.com>
2023-10-10 06:02:04 +0000

    Add changes to main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,12 +2,12 @@
 #define MAIN_H
 
 
-int _putchar(char letter);
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _islower(int character);
-int _isalpha(int character);
-int print_sign(int n);
+int _islower(int);
+int _isalpha(int);
+int print_sign(int);
 int _abs(int);
 int print_last_digit(int);
 
