4b3231d824fe59b1c83e542d100a545426f3e4c6
Jessy Baki <bakijessy@gmail.com>
2023-10-10 06:21:30 +0000

    Upd: Add changes to 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,21 +1,26 @@
 #include "main.h"
 
 /**
- * jack_bauer -  Prints last digit of a number.
- * Return: void
+ * jack_bauer -  Prints last digit of a number
+ * * Return: void
  */
+
 void jack_bauer(void)
 {
-	int h, m;
-	for (h = 0; h <= 23; h++)
+	int hours, mins;
+	
+	for (hours = 0; hours <= 23; hours++)
 	{
-		for (m = 0; m <= 59; m++)
+		for (mins = 0; mins <= 59; mins++)
 		{
-			_putchar((h / 10) + '0');
-			_putchar((h % 10) + '0');
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			
 			_putchar(':');
-			_putchar((m / 10) + '0');
-			_putchar((m % 10) + '0');
+			
+			_putchar((mins / 10) + '0');
+			_putchar((mins % 10) + '0');
+			
 			_putchar('\n');
 		}
 	}
