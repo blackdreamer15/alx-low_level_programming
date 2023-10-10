eddb845768fd55b88d1faf53afb0d9bb896ca721
Jessy Baki <bakijessy@gmail.com>
2023-10-10 06:28:30 +0000

    Update - Add a function to sum two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+vim #include "main.h"
+
+/**
+ * add -  A function to add two numbers
+ * @num1: first number
+ * @num2: second number
+ *
+ * Return - Return sum of num1 and num2
+ */
+int add(int num1, int num2)
+{
+	return (a + b);
+}
