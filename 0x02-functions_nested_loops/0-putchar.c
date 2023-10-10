2007a59cbdb9d3189a3e56e03f97663db4c1c39d
Jessy Baki <bakijessy@gmail.com>
2023-10-10 05:14:18 +0000

    Upd: Write a program to print the string _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - entry ponint of program
+ *
+ * Description: A program that prints "_putchar", followed by a new line
+ *
+ * Return: Always exit code 0 as success
+ */
+
+int main(void)
+{
+	char str[8] = "_putchar";
+
+	for (int i = 0; i < 8; i++)
+	{
+		_putchar(word[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
