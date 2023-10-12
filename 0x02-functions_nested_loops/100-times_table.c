dbb2b2be58cbb01a17bcf688aa930a918b2c18ce
Jessy Baki <bakijessy@gmail.com>
2023-10-12 17:45:07 +0000

    Add changes to 100-times_tables.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -24,50 +24,10 @@ void print_times_table(int n)
 					printf("%3d", result);
 					
 					if (j != n)
-					{
 						printf(", ");
 				}
-			}
+			
 			printf("\n");
 		}
-	}
-}
-
-
-/**
- * print_result - A function to print out to stdout
- *
- * @result: Value after multiplying
- * @j: Current loop iteration value
- */
-
-void print_result(int result, int j)
-{
-	int first_digit, second_digit, third_digit;
-
-	first_digit = result / 100;
-	second_digit = (result % 100) / 10;
-	third_digit = result % 10;
-
-	if (j == 0)
-		_putchar('0');
-
-	else if (result < 10)
-	{
-		_putchar(' ');
-		_putchar(' ');
-		_putchar('0' + third_digit);
-	}
-	else if (result < 100)
-	{
-		_putchar(' ');
-		_putchar('0' + second_digit);
-		_putchar('0' + third_digit);
-	}
-	else
-	{
-		_putchar('0' + first_digit);
-		_putchar('0' + second_digit);
-		_putchar('0' + third_digit);
 	}
 }
