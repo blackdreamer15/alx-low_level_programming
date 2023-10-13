3e8884f7bedd843687bc7f77dd770e2ca189709b
Jessy Baki <bakijessy@gmail.com>
2023-10-13 04:27:05 +0000

    Add C program for Fizz-Buzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print numbers from 1 to 100, but replace multiples
+ * of 3 with Fizz, multiples of 5 with Buzz, and multiples of both
+ * with FizzBuzz.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz");
+
+		else if (i % 5 == 0)
+			printf("Buzz");
+
+		else if (i % 3 == 0)
+			printf("Fizz");
+
+		else
+			printf("%d", i);
+
+		printf(" ");
+	}
+	printf("\n");
+
+	return (0);
+}
