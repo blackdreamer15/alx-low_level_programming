6b84bf23d9e852342d5c76a2e829c25872395295
Jessy Baki <bakijessy@gmail.com>
2023-10-13 02:13:27 +0000

    Add changes to 100-times_tables.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,38 +1,48 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * print_times_table - prints time table
- * @n : the time table size
+ * print_times_table - Print the `n` times table, starting with 0.
+ * Description: If `n` is greater than 15 or less than 0, print nothing.
+ * @n: int type number
  */
-
 void print_times_table(int n)
 {
-	int i, j, result = 0;
+	int i, j, result;
 
-	if (n < 15)
+	if (n > 15 || n < 0)
+		return;
+	for (i = 0; i <= n; i++)
 	{
-		for (i = 0; i <= n; i++)
+		for (j = 0; j <= n; j++)
 		{
-			for (j = 0; j <= n; j++)
+			result = i * j;
+			if (result > 99)
 			{
-				result = i * j;
-
-				if (j <= 0)
-					printf("%d", result);
-
-				else if ((result < 10))
-					printf("   %d", result);
-
-				else if ((result < 100))
-					printf("  %d", result);
-
-				else
-					printf(" %d", result);
-
-					if (j < n)
-						putchar(',');
+				_putchar(result / 100 + '0');
+				_putchar((result / 10 % 10) + '0');
+				_putchar(result % 10 + '0');
 			}
-			putchar('\n');
+			else if (result > 9)
+			{
+				_putchar(' ');
+				_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
+			}
+			else if (j != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(result + '0');
+			}
+			else
+				_putchar(result + '0');
+
+			if (j != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
+		_putchar('\n');
 	}
 }
