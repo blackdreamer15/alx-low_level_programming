76f29c1e5e9cca8c167f9d2152372bbfbe10a910
Jessy Baki <bakijessy@gmail.com>
2023-10-13 02:39:57 +0000

    Revert "Add changes to 104-fibonacci.c"
    This reverts commit 34465f97c3e9981410e6ffc76b72845f5ac7c128.

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,51 +1,38 @@
 #include <stdio.h>
 
 /**
- * main - Prints the first 98 Fibonacci numbers, starting with
- * 1 and 2, separated by a comma followed by a space.
+ * main - entry point
  *
- * Return: Always 0.
+ * Description: Write a program that prints the first 98 Fibonacci
+ * numbers, starting with 1 and 2, followed by a new line
+ *
+ * Return: Return 0 if success
  */
+
 int main(void)
 {
-	int count;
-	unsigned long fib1 = 0, fib2 = 1, sum;
-	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-	unsigned long half1, half2;
+	unsigned long int i;
+	unsigned long int first_term = 1;
+	unsigned long int second_term = 2;
+	unsigned long int next_term;
 
-	for (count = 0; count < 92; count++)
+	printf("%lu, ", first_term);
+	printf("%lu, ", second_term);
+
+	for (i = 3; i < 99; i++)
 	{
-		sum = fib1 + fib2;
-		printf("%lu, ", sum);
-
-		fib1 = fib2;
-		fib2 = sum;
-	}
-
-	fib1_half1 = fib1 / 10000000000;
-	fib2_half1 = fib2 / 10000000000;
-	fib1_half2 = fib1 % 10000000000;
-	fib2_half2 = fib2 % 10000000000;
-
-	for (count = 93; count < 99; count++)
-	{
-		half1 = fib1_half1 + fib2_half1;
-		half2 = fib1_half2 + fib2_half2;
-		if (fib1_half2 + fib2_half2 > 9999999999)
+		next_term = first_term + second_term;
+		if (i == 98)
 		{
-			half1 += 1;
-			half2 %= 10000000000;
+			printf("%lu", next_term);
+			continue;
 		}
+		printf("%lu, ", next_term);
 
-		printf("%lu%lu", half1, half2);
-		if (count != 98)
-			printf(", ");
-
-		fib1_half1 = fib2_half1;
-		fib1_half2 = fib2_half2;
-		fib2_half1 = half1;
-		fib2_half2 = half2;
+		first_term = second_term;
+		second_term = next_term;
 	}
 	printf("\n");
+
 	return (0);
 }
