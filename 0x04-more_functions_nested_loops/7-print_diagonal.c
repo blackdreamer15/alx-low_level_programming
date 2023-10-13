b6fdd050fa3f9458d42f8374d28c8b9b4e163938
Jessy Baki <bakijessy@gmail.com>
2023-10-13 03:59:52 +0000

    Add changes to variable names

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,9 +15,9 @@ void print_diagonal(int n)
 
 	if (n > 0)
 	{
-		for (j = 0; j < n; j++)
+		for (i = 0; i < n; i++)
 		{
-			for (i = 0; i <= j; i++)
+			for (j = 0; j <= i; j++)
 			{
 				if (i == j)
 				{
