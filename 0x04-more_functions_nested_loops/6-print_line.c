fe20ecff1dcdfa2456175097612bd327b75e0724
Jessy Baki <bakijessy@gmail.com>
2023-10-13 03:49:55 +0000

    Add C program to draw a straight line in  terminal

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line in the terminal
+ * using the character `_` followed by a new line.
+ * If n <= 0, only a new line character is printed.
+ *
+ * @n: Number of times the character _ should be printed.
+ *
+ * Return: void.
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
