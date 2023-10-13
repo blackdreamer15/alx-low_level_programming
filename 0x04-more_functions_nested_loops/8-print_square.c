19435a80012798867ca13185b463a51a63467b39
Jessy Baki <bakijessy@gmail.com>
2023-10-13 04:09:59 +0000

    Update: Add a C program to print a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_square - Prints a square, followed by a new line
+ * using the character `#` followed by a new line.
+ * If n <= 0, only a new line character is printed.
+ *
+ * @size: The size of the square to be printed
+ *
+ * Return: void.
+ */
+void print_square(int size)
+{
+	_putchar('\n');
+
+	int row, col;
+
+	if (size > 0)
+	{
+		for (row = 0; row < size; row++)
+		{
+			for (col = 0; col < size; col++)
+				_putchar('#');
+
+			if (row == size - 1)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
