46cd12589708aadd57ad517b6e410a26ad9fb115
Jessy Baki <bakijessy@gmail.com>
2023-10-17 04:01:26 +0000

    Add a C program to print a string to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout.
+ *
+ * @str: string to be printed
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
