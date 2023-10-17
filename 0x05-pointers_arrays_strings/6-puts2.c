156996b9930eee07ef1440a42cfb0c4ec0807409
Jessy Baki <bakijessy@gmail.com>
2023-10-17 04:34:54 +0000

    Add C program to print characters with even index

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string, starting with
+ * the first character, followed by a new line.
+ *
+ * @str: pointer to a char
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+	int len = strlen(str);
+
+	for (i = 0; i < len - 1; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
