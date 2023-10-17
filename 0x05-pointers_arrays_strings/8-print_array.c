1a5bb81e77cab6ec27ed527a6494cfd9df57bf66
Jessy Baki <bakijessy@gmail.com>
2023-10-17 05:32:25 +0000

    Add early return to catch error

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,7 +8,8 @@
  * The numbers should be displayed in the same order as they are
  * stored in the array. You are allowed to use printf.
  *
- * @b: second pointer to an int
+ * @a: pointer to an int
+ * @n: number of elements of the array to be printed
  *
  * Return: void
  */
@@ -16,6 +17,12 @@
 void print_array(int *a, int n)
 {
 	int i;
+
+	if (n <= 0)
+	{
+		printf("\n");
+		return;
+	}
 
 	for (i = 0; i < n; i++)
 	{
