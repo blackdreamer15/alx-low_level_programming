9e0ce8e95b73804e02911556a825d632385e3145
Jessy Baki <bakijessy@gmail.com>
2023-10-17 04:09:53 +0000

    Add a C program to print a string in reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - Prints a string, in reverse, followed by a new line.
+ *
+ * @s: Pointer to use
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int len = strlen(s);
+
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
