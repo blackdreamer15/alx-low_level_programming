dbb469d0d0461efc74040e0b7adfd69e86a52def
Jessy Baki <bakijessy@gmail.com>
2023-10-17 04:20:17 +0000

    Add a C program to reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - It reverses a string.
+ *
+ * @s: Pointer to string to be reversed
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int temp;
+	int len = strlen(s);
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+}
