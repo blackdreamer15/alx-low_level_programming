b99fa8728a1783c7f6d81f9e86e76b2facf2c0ca
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-10-20 06:43:43 +0000

    Add changes to 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,30 +1,31 @@
 #include "main.h"
 
 /**
- * print_number - A function that prints an integer
- *
- * @n: Integer variable
- *
- * Return - void
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
  */
-
 void print_number(int n)
 {
-	char negative_sign = '-';
+	unsigned int m, d, count;
 
 	if (n < 0)
 	{
-		_putchar(negative_sign);
-		n = -n;
+		_putchar(45);
+		m = n * -1;
 	}
-	else if (n == 0)
+	else
+		m = n;
+
+	d = m;
+	count = 1;
+
+	while (d > 9)
 	{
-		_putchar('0');
-		return;
+		d /= 10;
+		count *= 10;
 	}
 
-	if ((n / 10) > 0)
-		print_number(n / 10);
-
-	_putchar('0' + (n % 10));
+	for (; count >= 1; count /= 10)
+		_putchar(((m / count) % 10) + 48);
 }
