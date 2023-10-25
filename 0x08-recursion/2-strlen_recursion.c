85f20af4cfa4f262d963358024964388a9f557fe
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-10-25 16:11:47 +0000

    Fix: Measure length of a string recursively

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,12 +2,12 @@
 
 /**
  * _strlen_recursion - Measures the length of a string
- *
  * @s: string to be measured
- *
  * Return: length of string
  */
 int _strlen_recursion(char *s)
 {
-	return (strlen(s));
+	if (*s == '\0')
+		return (0);
+	return (1 + (_strlen_recursion(s + 1)));
 }
