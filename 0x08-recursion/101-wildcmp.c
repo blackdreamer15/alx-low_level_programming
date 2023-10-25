78f7747bb4a645b77419b1379776917552ebb117
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-10-25 17:43:39 +0000

    Remove trailing spaces

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -5,7 +5,7 @@
  * @s1: the first string
  * @s2: the second string
  * Return: 1 if the strings can be considered identical,
- * 		   otherwise return 0.
+ *		   otherwise return 0.
  */
 int wildcmp(char *s1, char *s2)
 {
