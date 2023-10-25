3ded14245fbb09a38fb3bd81ce89c19f6d06c447
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-10-25 16:07:23 +0000

    Add a C program to measure the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Measures the length of a string
+ *
+ * @s: string to be measured
+ *
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
