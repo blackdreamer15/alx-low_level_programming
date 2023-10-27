fd2e3b8ad0c4872c81753d9ef666ed2ecdcd8d7b
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-10-27 14:06:19 +0000

    Doc: Remove trailing space

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,7 +3,7 @@
 /**
  * main - prints the program's name, followed by a new line.
  *        If you rename the program, it will print the new
- * 		  name, without having to compile it again.
+ *		  name, without having to compile it again.
  * @argc: argument count
  * @argv: 1-D argument vector
  * Return: Always 0 (Success)
