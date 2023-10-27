c310264d9a09d98d0904e9d3049a47cce1444c73
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-10-27 14:24:44 +0000

    C code to multiply two arguments and print result

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - print the result of the multiplication of
+ *        two numbers, followed by a new line.
+ * @argc: argument count
+ * @argv: 1-D argument vector
+ * Return: 0 if Success, otherwise 1.
+ */
+int main(int argc, char const *argv[])
+{
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+	return (0);
+}
