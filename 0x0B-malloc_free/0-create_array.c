0283a56e9d2cbb0d9d0f2ea2b6d5e814d9e6f523
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-10-31 11:27:06 +0000

    DOc: Remove trailing spaces

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,26 +1,27 @@
 #include "main.h"
 
 /**
- * create_array - creates an array of chars, and initializes it with a specific char.
+ * create_array - creates an array of chars, and
+ *				  initializes it with a specific char.
  * @size: size of the array
  * @c: char to initialize the array
  * Return: pointer to the array, or NULL if it fails
  */
 char *create_array(unsigned int size, char c)
 {
-    char *arr;
-    unsigned int i;
+	char *arr;
+	unsigned int i;
 
-    if (size == 0)
-        return (NULL);
+	if (size == 0)
+		return (NULL);
 
-    arr = (char *)malloc(sizeof(char) * size);
+	arr = (char *)malloc(sizeof(char) * size);
 
-    if (arr == NULL)
-        return (NULL);
+	if (arr == NULL)
+		return (NULL);
 
-    for (i = 0; i < size; i++)
-        arr[i] = c;
+	for (i = 0; i < size; i++)
+		arr[i] = c;
 
-    return (arr);
+	return (arr);
 }
