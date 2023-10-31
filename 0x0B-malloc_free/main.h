e036a3a6e1e947ededf944f1ecfa35e4cd10bad1
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-10-31 15:02:35 +0000

    Add changes to main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -6,7 +6,6 @@
 #include <string.h>
 #include <ctype.h>
 
-
 char *create_array(unsigned int size, char c);
 
 char *_strdup(char *str);
@@ -16,5 +15,9 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 
 void free_grid(int **grid, int height);
+
+char *argstostr(int ac, char **av);
+
+char **strtow(char *str);
 
 #endif /* MAIN_H */
