a5fd8440e45eb4b7fa2b487fb81f0db4eeea6fd3
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-03 12:24:57 +0000

    Add changes to 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,36 +1,76 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
 /**
- * main - multiply 2 numbers as arguments
+ * print_free_and_exit98 - prints Error and exits
+ * @result: pointer to array of integers
+ * Return: void
+ */
+void print_free_and_exit98(int *result)
+{
+	printf("Error\n");
+	free(result);
+	exit(98);
+}
+
+/**
+ * print_and_exit98 - prints Error and exits
+ * Return: void
+ */
+void print_and_exit98(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * main - multiplies two positive numbers
  * @argc: number of arguments
- * @argv: arguments
- * Return: 0 success
+ * @argv: array of pointers to arguments
+ *
+ * Return: 0 on success, 98 on failure
  */
 int main(int argc, char *argv[])
 {
-	unsigned long result;
-	int i, j;
+	int i, j, len1 = strlen(argv[1]), len2 = strlen(argv[2]), len = len1 + len2;
+	int *result;
 
 	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+		print_and_exit98();
 
-	for (i = 1; i < argc; i++)
+	result = calloc(sizeof(*result), len);
+	if (result == NULL)
+		print_and_exit98();
+
+	for (i = len1 - 1; i >= 0; i--)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		if (!isdigit(argv[1][i]))
+			print_free_and_exit98(result);
+
+		for (j = len2 - 1; j >= 0; j--)
 		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
+			if (!isdigit(argv[2][j]))
+				print_free_and_exit98(result);
+
+			result[i + j + 1] += (argv[1][i] - '0') * (argv[2][j] - '0');
+			if (result[i + j + 1] > 9)
 			{
-				printf("Error\n");
-				exit(98);
+				result[i + j] += result[i + j + 1] / 10;
+				result[i + j + 1] %= 10;
 			}
 		}
 	}
-	result = atol(argv[1]) * atol(argv[2]);
-	printf("%lu\n", result);
+	if (*result == 0)
+		i = 1;
+	else
+		i = 0;
+	for (; i < len; i++)
+		printf("%i", result[i]);
+
+	printf("\n");
+	free(result);
 
 	return (0);
 }
