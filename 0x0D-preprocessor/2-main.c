1764490f838cabf5076a6d659af54f4360b2eb3a
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-06 15:58:38 +0000

    Add a C program to print name of the file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to print name of the file
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
