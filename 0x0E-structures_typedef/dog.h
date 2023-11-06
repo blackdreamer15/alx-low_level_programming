1c926acccfce20729b94f198056d23dca569040b
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-06 20:28:53 +0000

    Add changes to dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,29 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
-	char* name;
+/**
+ * struct dog - dog structure
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: dog structure having members: name, age and owner
+ */
+struct dog
+{
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
 
 #endif /*DOG_H*/
