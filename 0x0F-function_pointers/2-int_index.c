079dff712e5b48668546f3afe1ea815cf2e8a3b7
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-09 22:51:35 +0000

    Add a C program to search for an integer in an array

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer in an array of integers
+ * @array: array of integers
+ * @size: size of array
+ * @cmp: pointer to function to be used to compare values
+ * Return: index of first element for which cmp function does not return 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, return_val;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		return_val = cmp(array[i]);
+
+		if (return_val != 0)
+			return (i);
+	}
+
+	return (-1);
+}
