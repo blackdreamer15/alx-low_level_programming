f932afdb625632346ec7f46beee0dbb2441606a5
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-09 23:57:52 +0000

    Add additional header files to 3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,6 +1,10 @@
 #ifndef CALC_H
 #define CALC_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct op - Struct op
  *
@@ -12,8 +16,6 @@ typedef struct op
 	char *op;
 	int (*f)(int a, int b);
 } op_t;
-
-
 
 int (*get_op_func(char *s))(int, int);
 
