dbfa23e4a01126ab0f5b9e627ab6da83f7249728
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-10 00:13:11 +0000

    Add function definitions for all op functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two numbers and returns the result
+ * @a: first operand
+ * @b: second operand
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substracts two numbers and returns the result
+ * @a: first operand
+ * @b: second operand
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply two numbers and returns the result
+ * @a: first operand
+ * @b: second operand
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two numbers and returns the result
+ * @a: first operand
+ * @b: second operand
+ * Return: result of the division of a by b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - divides two numbers and returns the remainder (modulo)
+ * @a: first operand
+ * @b: second operand
+ * Return: remainder of the division of a by b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
