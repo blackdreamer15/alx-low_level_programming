4d6833f2b45a84deaa14e1c4e81d0b7378f1f0f0
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-10 00:38:13 +0000

    Add main entry file for task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,39 @@
+#include "3-calc.h"
+
+/**
+ * main - entry point to perform arithmetic operations
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: Exit with 0 as success
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	int num1, num2;
+	char *op_func;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	op_func = argv[2];
+	num2 = atoi(argv[3]);
+
+	if (get_op_func(op_func) == NULL || op_func[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*op_func == '/' || *op_func == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(op_func)(num1, num2));
+
+	return (0);
+}
