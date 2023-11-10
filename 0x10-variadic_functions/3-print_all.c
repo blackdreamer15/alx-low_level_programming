bacb252974cbbe3897ad6dbd8334cd322ff5e78d
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-10 03:47:05 +0000

    Add changes to 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,39 +8,44 @@
  */
 void print_all(const char *const format, ...)
 {
-	va_list args;
-	unsigned int i;
+	int i = 0;
 	char *str, *separator = "";
+
+	va_list args;
 
 	va_start(args, format);
 
-	if (format != NULL)
+	if (format)
 	{
-		for (i = 0; format[i]; i++)
+		while (format[i])
 		{
 			switch (format[i])
 			{
 			case 'c':
-				printf("%c", va_arg(args, int));
+				printf("%s%c", separator, va_arg(args, int));
 				break;
 			case 'i':
-				printf("%d", va_arg(args, int));
+				printf("%s%d", separator, va_arg(args, int));
 				break;
 			case 'f':
-				printf("%f", va_arg(args, double));
+				printf("%s%f", separator, va_arg(args, double));
 				break;
 			case 's':
-				str = (str != NULL) ? va_arg(args, char *) : "(nil)";
-				printf("%s", str);
+				str = va_arg(args, char *);
+				if (!str)
+					str = "(nil)";
+				printf("%s%s", separator, str);
 				break;
 			default:
+				i++;
 				continue;
 			}
 
-			if (format[i + 1] != '\0')
-				printf("%s", separator);
+			separator = ", ";
+			i++;
 		}
 	}
+
 	printf("\n");
 
 	va_end(args);
