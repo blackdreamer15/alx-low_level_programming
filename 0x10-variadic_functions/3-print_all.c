bff1c2df24ee7f0bc564c07a222a7ec2591be147
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-10 02:58:31 +0000

    Add changes to 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -14,36 +14,30 @@ void print_all(const char *const format, ...)
 
 	va_start(args, format);
 
-	if (format != NULL)
+	for (i = 0; format != NULL && format[i]; i++)
 	{
-		for (i = 0; format[i]; i++)
+		switch (format[i])
 		{
-			switch (format[i])
-			{
-			case 'c':
-				printf("%c", va_arg(args, int));
-				break;
-			case 'i':
-				printf("%i", va_arg(args, int));
-				break;
-			case 'f':
-				printf("%f", va_arg(args, double));
-				break;
-			case 's':
-				str = va_arg(args, char *);
-
-				if (str == NULL)
-					str = "(nil)";
-
-				printf("%s", str);
-				break;
-			default:
-				continue;
-			}
-
-			if (format[i + 1] != '\0')
-				printf(", ");
+		case 'c':
+			printf("%c", va_arg(args, int));
+			break;
+		case 'i':
+			printf("%i", va_arg(args, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(args, double));
+			break;
+		case 's':
+			str = va_arg(args, char *);
+			str = (str != NULL) ? str : "(nil)";
+			printf("%s", str);
+			break;
+		default:
+			continue;
 		}
+
+		if (format[i + 1] != '\0')
+			printf(", ");
 	}
 	printf("\n");
 
