2055fc2348d76d02f7ebe44f6cc66560da721d46
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-18 05:32:37 +0000

    Upd: dd a program to free a list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,4 +8,18 @@
  */
 void free_list(list_t *head)
 {
+    list_t *temp;
+
+    if (head == NULL)
+        return;
+
+    while (head != NULL)
+    {
+        temp = head;
+        head = head->next;
+        free(temp->str);
+        free(temp);
+    }
+
+    free(head);
 }
