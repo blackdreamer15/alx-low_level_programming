75ddcbae4a98b9309cfd148d7069204f21831440
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-18 05:05:45 +0000

    Update: Add changes to 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,11 +8,11 @@
  */
 size_t print_list(const list_t *h)
 {
-	register int count = 0;
+	size_t count = 0;
 
 	while (h)
 	{
-		if (!h->str)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
