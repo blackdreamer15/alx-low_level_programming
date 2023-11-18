5e9ecb163a1e87633ffd574e568dd8be42f778e9
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-18 05:31:07 +0000

    Upd: dd a program to add a node at the end of a linked list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,4 +9,40 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
+	list_t *new_node;
+	list_t *tail = *head;
+	char *new_str;
+
+	if (str == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_str = strdup(str);
+
+	if (new_str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->str = new_str;
+	new_node->len = strlen(new_str);
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (*head);
+	}
+
+	while (tail->next != NULL)
+		tail = tail->next;
+
+	tail->next = new_node;
+
+	return (tail);
 }
