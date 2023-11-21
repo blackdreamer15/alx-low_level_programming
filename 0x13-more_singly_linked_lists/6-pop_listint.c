7027c63c4ebe17e80055eea56295139cbafaa526
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-21 22:02:30 +0000

    Add starter files for tasks

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of a listint_t linked list
+ * @head: pointer to the head of the list
+ * Return: the head node's data (n)
+ */
+int pop_listint(listint_t **head)
+{
+}
---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * sum_listint - returns the sum of all the data (n) of a listint_t list
+ * @head: pointer to the head of the list
+ * Return: the sum of all the data (n) of a listint_t list
+ */
+int sum_listint(listint_t *head)
+{
+}
