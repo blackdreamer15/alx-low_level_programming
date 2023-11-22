fd62a03f9a31c6d7a51addd6b7fa24df3688bed9
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-22 05:27:47 +0000

    Update 8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -7,4 +7,13 @@
  */
 int sum_listint(listint_t *head)
 {
+        int sum = 0;
+
+        while (head)
+        {
+                sum += head->n;
+                head = head->next;
+        }
+
+        return (sum);
 }
