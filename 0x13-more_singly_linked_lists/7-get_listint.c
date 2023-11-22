d4106ce9f6c2a45c129d0ce4d6b672def53fdab2
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-22 05:24:47 +0000

    Update 7-get_listint.c

--- 0x13-more_singly_linked_lists/7-get_listint.c
+++ 0x13-more_singly_linked_lists/7-get_listint.c
@@ -8,4 +8,15 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+        unsigned int node;
+
+        for (node = 0; node < index; node++)
+        {
+                if (head == NULL)
+                        return (NULL);
+
+                head = head->next;
+        }
+
+        return (head);
 }
