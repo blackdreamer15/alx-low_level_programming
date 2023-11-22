46655126e095fd2526eb074e1bc602ca79ea8d8a
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-22 05:56:04 +0000

    Add changes to task files according to Betty linter

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,13 +8,13 @@
  */
 size_t listint_len(const listint_t *h)
 {
-        size_t elements = 0;
+	size_t elements = 0;
 
-        while (h)
-        {
-                elements++;
-                h = h->next;
-        }
+	while (h)
+	{
+		elements++;
+		h = h->next;
+	}
 
-        return (elements);
+	return (elements);
 }
