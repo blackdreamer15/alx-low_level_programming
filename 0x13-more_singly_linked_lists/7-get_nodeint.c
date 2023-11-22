edfd742395e75f19bf186d8bf3a8c83ec9ffce00
blackdreamer15 <bakijessy@gmail.com>
2023-11-22 19:47:17 +0000

    Fix: Refactor codebase to pass remaining test cases

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,15 +1,11 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - Write a function that returns the nth node of a
- * listint_t linked list
- *
+ * get_nodeint_at_index - returns the nth node of a listint_t linked list
  * @head: Pointer to head of list
  * @index: the index of the node, starting at 0
- *
  * Return: if the node does not exist, return NULL
  */
-
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int node = 0;
