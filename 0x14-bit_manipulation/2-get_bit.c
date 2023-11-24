e510fc310dd901deeb024b5d6e9cef12ac6596f2
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-24 02:57:31 +0000

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,11 +2,22 @@
 
 /**
  * get_bit - returns the value of a bit at a given index.
- * @n:
- * @index: the index, starting from 0 of the bit you want to get
- * Return: the value of the bit at index index or -1 if an error occured
-*/
+ * @n: number to check bits in
+ * @index: index at which to check bit
+ * Return: value of the bit, or -1 if error occurs.
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned long int divisor, check;
 
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+
+	divisor = 1 << index;
+	check = n & divisor;
+
+	if (check == divisor)
+		return (1);
+
+	return (0);
 }
