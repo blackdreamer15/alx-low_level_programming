93a777f8954d0fa996425d4b52409f88cbedbcfd
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-24 02:05:27 +0000

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,1 +1,14 @@
+#include "main.h"
 
+/**
+ * print_binary - prints the binary representation of a number.
+ *                You are not allowed to use arrays
+ *                You are not allowed to use mallo
+ *                You are not allowed to use the % or / operators
+ * @n:
+ *Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	
+}
