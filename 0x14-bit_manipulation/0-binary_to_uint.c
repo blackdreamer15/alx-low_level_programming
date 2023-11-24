c7955c93749734d96e82e1698011ad9ce214bc2b
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-24 02:34:36 +0000

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,20 +10,20 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
-	unsigned int i, len;
+	int i;
 
 	if (b == NULL)
 		return (1);
 
-	len = strlen(b);
-
-	for (i = 0; i < len; i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] != '0' || b != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-
-		b <<= 1;
-		num += b[i] - '0';
+		
+		num <<= 1;
+		
+		if (b[i] == '1')
+			num += 1;
 	}
 
 	return (num);
