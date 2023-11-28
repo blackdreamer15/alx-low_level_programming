2913440c2746f3621bf03edf1b2ecf5a95a7fff3
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-28 04:08:04 +0000

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,5 +14,21 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-  
+  char *buffer;
+  int r_len, w_len, fd;
+
+  if (filename == NULL)
+    return (0);
+
+  fd = open(filename, O_RDONLY);
+
+  if (fd < 0)
+    return (0);
+
+  buffer = malloc(sizeof(char) * letters);
+
+  if (buffer == NULL)
+    return (0);
+
+  r_len = write(filename, buffer, letters);
 }
