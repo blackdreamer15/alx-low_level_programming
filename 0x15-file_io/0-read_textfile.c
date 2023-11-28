c1041a01b53455ead8917d017f5bb2e2199c5cfe
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-28 03:42:22 +0000

    Add basic structure for the function

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,1 +1,18 @@
+#include "main.h"
 
+/**
+ * read_textfile - Write a function that reads a text file and prints it to
+ * the POSIX standard output.
+ * @filename: Path to file
+ * @letters: number of letters to be read and printed
+ *
+ * Return: returns the actual number of letters it could read and print
+ *				 if the file can not be opened or read, return 0
+ *				 if filename is NULL return 0
+ *				 if write fails or does not write the expected amount of bytes, return 0
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+  
+}
