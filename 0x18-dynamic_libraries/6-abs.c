7d5cbd1044e546139711480b60377a26d80134a9
blackdreamer15 <bakijessy@gmail.com>
2024-01-18 05:39:31 +0000

    Update: Add implementation of abs

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - A function to find the absolute value of an integer
+ * @num: A parameter capturing integer value
+ *
+ * Return: the absolute value
+ */
+
+int _abs(int num)
+{
+	if (num < 0)
+		return (-num);
+	else
+		return (num);
+}
