6714420d1c0d868e5c662b847478ce64573f19fc
blackdreamer15 <bakijessy@gmail.com>
2024-01-18 05:40:54 +0000

    Update: Add implementation of strcmp

---
+++ 0x18-dynamic_libraries/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strcmp - A function that compares two strings
+ *
+ * @s1: Pointer to first string
+ * @s2: Pointer to second string
+ *
+ * Return: 0, if the s1 and s2 are equal
+ * a negative value if s1 is less than s2;
+ * a positive value if s1 is greater than s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
